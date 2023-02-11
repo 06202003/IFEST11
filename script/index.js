ac953d0bab3a21743767ea7fc54d5efe591acde1
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2023-02-11 23:39:48 +0700

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -3,15 +3,7 @@ var logo = document.getElementById('logo');
 window.addEventListener('scroll', function () {
   if (window.pageYOffset > 100) {
     nav.classList.add('bgnav', 'shadow');
-    if (logo && logo.style) {
-      logo.style.height = '75px';
-      logo.style.width = '75px';
-    }
   } else {
     nav.classList.remove('bgnav', 'shadow');
-    if (logo && logo.style) {
-      logo.style.height = '150px';
-      logo.style.width = '150px';
-    }
   }
 });
